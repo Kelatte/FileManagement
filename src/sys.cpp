970ce5aec4969986fe0896ba3388e1c84b5e9ae1
wknclizu_surface <wknclizu@gmail.com>
2023-12-26 20:03:14 +0800

    增加了注释

--- src/sys.cpp
+++ src/sys.cpp
@@ -83,24 +83,38 @@ int sys_close(unsigned int fd) {
  * @brief 通过文件描述符读取指定长度到buf中
  */
 int sys_read(unsigned int fd, char* buf, int count) {
-  struct file* file;
-  struct m_inode* inode;
+  struct file* file;  // 文件结构体指针，用于表示文件
+  struct m_inode* inode;  // i节点结构体指针，用于表示文件的元数据信息
 
+  // 检查文件描述符的有效性，读取长度是否合法，以及文件结构体是否存在
   if (fd >= NR_OPEN || count < 0 || !(file = fileSystem->filp[fd]))
     return -EINVAL;
+
+  // 如果读取长度为0，直接返回0表示已经读取完文件
   if (!count) return 0;
-  // verify_area(buf, count);
+
+  // 获取文件的i节点信息
   inode = file->f_inode;
+
   /*目前允许读目录和普通文件*/
+  // 判断文件类型是否为目录或普通文件
   if (S_ISDIR(inode->i_mode) || S_ISREG(inode->i_mode)) {
+    // 如果读取长度加上文件当前位置超过文件大小，则调整读取长度
     if (count + file->f_pos > inode->i_size)
       count = inode->i_size - file->f_pos;
+    
+    // 如果调整后的读取长度小于等于0，表示已经读取完文件，直接返回0
     if (count <= 0) return 0;
+
+    // 调用文件读取函数，实际读取文件内容到缓冲区
     return file_read(inode, file, buf, count);
   }
+
+  // 如果文件类型不是目录或普通文件，输出错误信息并返回错误码
   printf("(Read)inode->i_mode=%06o\n\r", inode->i_mode);
   return -EINVAL;
 }
+
 
 /*
  * @brief 将buf中指定长度的内容写入到文件描述符所指的文件中,只允许写普通文件
@@ -119,111 +133,155 @@ int sys_write(unsigned int fd, char* buf, int count) {
   return -EINVAL;
 }
 
+/*
+ * @brief 获取给定i节点所对应文件的工作目录路径
+ * @param[in] inode 指向文件i节点的指针
+ * @param[out] out 存储工作目录路径的字符串
+ * @return 0 表示成功，-1 表示失败
+ */
 int sys_get_work_dir(struct m_inode* inode, string& out) {
   int inum = inode->i_num;
+
+  // 如果给定i节点是根目录的i节点，设置输出路径为"/"表示根目录，并返回成功
   if (inum == fileSystem->root->i_num) {
     out = "/";
     return 0;
   }
+
+  // 初始化输出字符串为空
   out = "";
   char name[20];
   struct m_inode* fa;
-  inode->i_count++;  //之后会iput一次
+  inode->i_count++;  // 之后会iput一次
 
+  // 循环直到回溯到根目录的i节点
   while (inode->i_num != fileSystem->root->i_num) {
+    // 获取当前i节点的文件名
     if (!get_name(inode, name, 20)) {
       return -1;
     }
 
+    // 将文件名转为字符串，并在前面添加"/"
     string s(&name[0], &name[strlen(name)]);
     s.insert(0, "/");
+    
+    // 将当前文件名添加到输出路径的前面
     out.insert(0, s);
+
+    // 获取当前i节点的父目录i节点
     if (!(fa = get_father(inode))) {
       return -1;
     };
 
+    // 将当前i节点引用计数加1，因为后面会调用iput释放引用
+    inode->i_count++;
+
+    // 释放当前i节点的引用，切换到父目录i节点
     iput(inode);
     inode = fa;
   }
+
   return 0;
 }
 
+
 // ls命令 显示当前目录下所有文件
 int cmd_ls(string s) {
-  bool flag = false;
-  int entries;
-  int block, i;
+  bool flag = false;  // 标记是否已输出表头信息
+  int entries;  // 目录中的目录项总数
+  int block, i;  // 当前目录项所在块号和计数器
   string out = "";
-  struct buffer_head* bh;
-  struct dir_entry* de;
+  struct buffer_head* bh;  // 缓冲区头指针，用于读取文件块数据
+  struct dir_entry* de;  // 目录项结构体指针，表示目录中的一个文件或子目录
   struct m_inode* dir =
-      ((s == "" || s == "-l") ? fileSystem->current : get_inode(s.c_str()));
+      ((s == "" || s == "-l") ? fileSystem->current : get_inode(s.c_str()));  // 获取当前目录的i节点
+
+  // 如果目录不存在，返回目录不存在的错误码
   if (!dir) {
     return -ENOENT;
   }
-  // 文件大小可能超过1个block
+
+  // 计算目录中的目录项总数
   entries = dir->i_size / (sizeof(struct dir_entry));
 
+  // 获取目录的第一个数据块
   block = dir->i_zone[0];
   if (block <= 0) {
     return -EPERM;
   }
+
   int count = 0;
+  // 读取目录项所在块的数据
   bh = bread(block);
   i = 0;
   de = (struct dir_entry*)bh->b_data;
+
+  // 遍历目录中的所有目录项
   while (i < entries) {
-    /*一个目录块读取完毕,则换下一个目录寻找*/
+    // 如果一个目录块读取完毕，则切换到下一个目录块
     if ((char*)de >= BLOCK_SIZE + bh->b_data) {
       brelse(bh);
       block = bmap(dir, i / DIR_ENTRIES_PER_BLOCK);
       bh = bread(block);
+      
+      // 如果下一个目录项读取失败，则跳过该目录
       if (bh == NULL || block <= 0) {
-        /*如果下一个目录项读取失败，则跳过该目录*/
         i += DIR_ENTRIES_PER_BLOCK;
         continue;
       } else
         de = (struct dir_entry*)bh->b_data;
     }
-    /*de->inode =0 代表该目录是空的*/
+    
+    // 如果当前目录项不是".."、"."且对应的i节点存在
     if (strcmp(de->name, "..") && strcmp(de->name, ".") && de->inode != 0) {
       m_inode* inode = iget(0, de->inode);
-      if (inode) {
-        if (s == "-l") {
-          if (!flag) {
-            printf("%8s %13s %14s %35s\n", "mode", "size", "name",
-                   "最后修改时间");
-            flag = true;
-          }
-          printf("%10s %13s ", GetFileMode(inode->i_mode).c_str(),
-                 GetFileSize(inode->i_size).c_str());
-          if (S_ISDIR(inode->i_mode)) {
-            printfc(FG_BLACK, BG_GREEN, "%14s", de->name);
-          } else {
-            printfc(FG_WHITE, "%14s", de->name);
-          }
-          printf(" %35s", longtoTime(inode->i_mtime));
-        } else {
-          if (S_ISDIR(inode->i_mode))
-            pdirc(de->name);
-          else
-            pfilec(de->name);
-          printf("  ");
+
+      // 如果需要显示详细信息（-l选项）
+      if (s == "-l") {
+        if (!flag) {
+          // 输出表头信息
+          printf("%8s %13s %14s %35s\n", "mode", "size", "name",
+                 "最后修改时间");
+          flag = true;
         }
+
+        // 输出详细信息，包括文件模式、文件大小、文件名和最后修改时间
+        printf("%10s %13s ", GetFileMode(inode->i_mode).c_str(),
+               GetFileSize(inode->i_size).c_str());
+        if (S_ISDIR(inode->i_mode)) {
+          printfc(FG_BLACK, BG_GREEN, "%14s", de->name);
+        } else {
+          printfc(FG_WHITE, "%14s", de->name);
+        }
+        printf(" %35s", longtoTime(inode->i_mtime));
+      } else {
+        // 如果不需要显示详细信息，只输出文件名或目录名
+        if (S_ISDIR(inode->i_mode))
+          pdirc(de->name);
+        else
+          pfilec(de->name);
+        printf("  ");
       }
+
       count++;
-      iput(inode);
+      iput(inode);  // 释放i节点
     }
     de++;
     i++;
   }
+
+  // 释放缓冲区头
   brelse(bh);
+
+  // 如果目录为空，输出提示信息
   if (count <= 0) {
     pinfoc("该目录为空");
   }
+
   cout << endl;
   return 0;
 }
+
 
 /*stat命令，显示文件详细信息*/
 int cmd_stat(string path) {
@@ -232,22 +290,34 @@ int cmd_stat(string path) {
   struct m_inode *dir, *inode;
   struct buffer_head *bh, *dir_block;
   struct dir_entry* de;
+
+  // 获取目录的i节点和文件名信息
   if (!(dir = dir_namei(path.c_str(), &namelen, &basename))) return -ENOENT;
+
+  // 如果文件名为空，说明路径不存在，释放目录i节点并返回错误码
   if (!namelen) {
     iput(dir);
     return -ENOENT;
   }
+
+  // 查找目录项并获取相应的目录块
   bh = find_entry(&dir, basename, namelen, &de);
+
+  // 如果目录项查找失败，释放目录i节点并返回错误码
   if (!bh) {
     iput(dir);
     return -ENOENT;
   }
+
+  // 获取文件对应的i节点
   if (!(inode = iget(dir->i_dev, de->inode))) {
     iput(dir);
     brelse(bh);
     return -EPERM;
   }
 
+  // 输出文件详细信息，包括文件名、设备号、文件模式、硬链接数、i节点号、
+  // 第一个数据块号、文件大小和最后修改时间等
   cout << "name: " << string(basename) << endl;
   cout << "dev: " << inode->i_dev << endl;
   cout << "mode: " << GetFileMode(inode->i_mode) << endl;
@@ -258,94 +328,138 @@ int cmd_stat(string path) {
   // cout << "最后访问时间: " << longtoTime(inode->i_atime) << endl;
   cout << "最后修改时间: " << longtoTime(inode->i_mtime) << endl;
   // cout << "i节点自身最终被修改时间: " << longtoTime(inode->i_ctime) << endl;
+
   return 0;
 }
+
 
 /*cd命令，移动工作目录*/
 int cmd_cd(string path) {
   struct m_inode* dir = NULL;
+
+  // 获取目标目录的i节点
   if ((dir = get_inode(path.c_str()))) {
+    // 释放当前工作目录的i节点
     iput(fileSystem->current);
+    
+    // 将当前工作目录设置为目标目录
     fileSystem->current = dir;
+
+    // 更新文件系统中的当前工作目录路径
     sys_get_work_dir(dir, fileSystem->name);
   } else {
-    return -ENOENT;
+    return -ENOENT;  // 如果目标目录不存在，返回错误码
   }
-  return 0;
+
+  return 0;  // 返回0表示cd命令执行成功
 }
 
 /*pwd命令，输出当前路径名*/
 int cmd_pwd() {
-  struct m_inode* dir = NULL;
-  string pwd = "";
+  struct m_inode* dir = NULL;  // 目录的i节点指针
+  string pwd = "";  // 存储当前路径名的字符串
+
+  // 调用sys_get_work_dir函数获取当前工作目录的路径
   if (sys_get_work_dir(fileSystem->current, pwd)) {
+    // 如果成功获取路径，则以颜色输出当前路径名
     ppathc(pwd);
-    return 0;
+    return 0;  // 返回0表示pwd命令执行成功
   }
-  return -EPERM;
+
+  return -EPERM;  // 返回错误码表示获取路径失败
 }
 
 /*cat命令，输出指定文件的所有内容*/
 int cmd_cat(string path) {
   int fd, size, i;
   struct m_inode* inode;
+
+  // 获取指定文件的i节点
   if (!(inode = get_inode(path.c_str()))) return -ENOENT;
+
+  // 如果指定的路径为目录，执行cmd_stat命令以显示目录详细信息
   if (inode->i_mode == S_IFDIR) return cmd_stat(path);
 
+  // 打开文件以只读方式
   fd = sys_open(path, O_RDONLY, S_IFREG);
   if (fd < 0) {
     iput(inode);
     return fd;
   }
-  size = inode->i_size;
+
+  // 获取文件大小
+
+  // 分配足够大的缓冲区来存储文件内容
   char* buf = new char[size + 2];
+
+  // 读取文件内容到缓冲区
   if ((i = sys_read(fd, buf, size)) < 0) {
     sys_close(fd);
     iput(inode);
+    delete[] buf;
     return i;
   }
+
+  // 根据文件类型输出不同的信息
   if (size == 0) {
     pinfoc("文件为空\n");
   } else if (S_ISREG(inode->i_mode)) {
+    // 如果是普通文件，输出文件大小和十六进制表示的内容
     pinfoc("文件大小：" + GetFileSize(size) + '\n');
     for (i = 0; i < size; ++i) {
       printf("%x", buf[i]);
     }
     printf("\n");
-  } else  //其他文件类型以二进制流输出
-  {
+  } else {
+    // 对于其他文件类型，以十六进制流形式输出文件内容
     pinfoc("目录大小：" + GetFileSize(size) + '\n');
     for (i = 0; i < size; ++i) {
       printf("%x", buf[i]);
     }
     printf("\n");
   }
+
+  // 关闭文件，释放i节点，释放缓冲区
   sys_close(fd);
   iput(inode);
   delete[] buf;
-  return 0;
+
+  return 0;  // 返回0表示cat命令执行成功
 }
+
 
 /*vi指令，可以在文件末尾增添内容*/
 int cmd_vi(string path) {
   int fd, size, i;
   string in = "";
+
+  // 以追加方式打开文件
   fd = sys_open(path, O_APPEND, S_IFREG);
 
+  // 如果打开文件失败，返回相应的错误码
   if (fd < 0) {
     return fd;
   }
+
+  // 提示用户输入要追加的内容
   printfc(FG_YELLOW, "请输入内容：");
   cin >> in;
-  getchar();
+  getchar();  // 读取换行符
+
+  // 将用户输入的内容追加到文件末尾
   i = sys_write(fd, (char*)in.c_str(), in.length());
+
+  // 如果写入操作失败，关闭文件并返回错误码
   if (i < 0) {
     sys_close(fd);
     return i;
   }
+
+  // 提示用户添加成功，并关闭文件
   psucc("\n添加成功");
   sys_close(fd);
-  return 0;
+
+  return 0;  // 返回0表示vi指令执行成功
 }
 
 /*mkdir命令，创建一个新的文件夹（目录）*/
@@ -355,13 +469,17 @@ int cmd_mkdir(const char* pathname, int mode) {
   struct m_inode *dir, *inode;
   struct buffer_head *bh, *dir_block;
   struct dir_entry* de;
-  // dir是找到的父目录
+
+  // 获取父目录的i节点和新目录的基本信息
   if (!(dir = dir_namei(pathname, &namelen, &basename))) return -ENOENT;
+
+  // 如果目录名为空，说明路径不存在，释放目录i节点并返回错误码
   if (!namelen) {
     iput(dir);
     return -ENOENT;
   }
-  //判断要增加的目录是否已经存在
+
+  // 判断要创建的目录是否已经存在
   bh = find_entry(&dir, basename, namelen, &de);
   if (bh) {
     brelse(bh);
@@ -369,25 +487,31 @@ int cmd_mkdir(const char* pathname, int mode) {
     return -EEXIST;
   }
 
-  //开始创建，首先创建子目录的inode节点
+  // 开始创建新目录，首先分配新的i节点
   inode = new_inode(dir->i_dev);
 
+  // 如果分配i节点失败，释放父目录i节点并返回错误码
   if (!inode) {
     iput(dir);
     return -ENOSPC;
   }
+
+  // 设置新目录的基本属性，包括大小、修改时间等
   inode->i_size = 32;
   inode->i_dirt = 1;
   inode->i_mtime = inode->i_atime = CurrentTime();
 
-  //为子目录创建第一个数据块，它将包含两个子目录项 . 和 ..
+  // 为新目录创建第一个数据块，包含两个子目录项 . 和 ..
   if (!(inode->i_zone[0] = new_block(inode->i_dev))) {
     iput(dir);
     inode->i_nlinks--;
     iput(inode);
     return -ENOSPC;
   }
+
   inode->i_dirt = 1;
+
+  // 读取新目录的数据块，准备插入两个子目录项
   if (!(dir_block = bread(inode->i_zone[0]))) {
     iput(dir);
     free_block(inode->i_dev, inode->i_zone[0]);
@@ -395,33 +519,38 @@ int cmd_mkdir(const char* pathname, int mode) {
     iput(inode);
     return -1;
   }
+
   de = (struct dir_entry*)dir_block->b_data;
-  /*加入. 和 .. 两个子目录*/
+
+  // 加入 . 和 .. 两个子目录项
   de->inode = inode->i_num;
   strcpy(de->name, ".");
   de++;
   de->inode = dir->i_num;
   strcpy(de->name, "..");
 
-  /*由于一个目录节点新建时，有父目录指向它，加上 .
-   * 目录项指向自己，故i_nlinks=2*/
+  // 由于一个目录节点新建时，有父目录指向它，加上 . 目录项指向自己，故i_nlinks=2
   inode->i_nlinks = 2;
   dir_block->b_dirt = 1;
   brelse(dir_block);
+
+  // 设置新目录的文件模式
   inode->i_mode = mode;
   inode->i_dirt = 1;
-  //然后将子目录插入到父目录中
+
+  // 将新目录插入到父目录中
   bh = add_entry(dir, basename, namelen, &de);
 
-  if (!bh) {  //增加子目录失败，释放所有资源
+  // 如果插入子目录项失败，释放所有资源
+  if (!bh) {
     iput(dir);
     free_block(inode->i_dev, inode->i_zone[0]);
     inode->i_nlinks = 0;
     iput(inode);
     return -ENOSPC;
   }
-  // cout << "add_entry successful" << endl;
-  //终于创建成功，设定初始值
+
+  // 插入子目录项成功，设定初始值，释放资源
   de->inode = inode->i_num;
   bh->b_dirt = 1;
   dir->i_nlinks++;
@@ -429,8 +558,8 @@ int cmd_mkdir(const char* pathname, int mode) {
   iput(dir);
   iput(inode);
   brelse(bh);
-  // psucc("文件夹成功创建");
-  return 0;
+
+  return 0;  // 返回0表示mkdir命令执行成功
 }
 
 /*touch命令,创建一个普通的文件节点*/
